c9ed12dd46838e852876f4edac136352145a7123
Twisted Harland <tharland@mi-t3.msk.21-school.ru>
2022-03-29 13:59:48 +0300

    ho

--- error/error.c
+++
@@ -1,6 +0,0 @@
-#include "../cub.h"
-
-int error_mess(char *error)
-{
-	return (1);
-}
--- libft/ft_isalpha.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-int	ft_isalpha(int s)
-{
-	if ((s >= 65 && s <= 90) || (s >= 97 && s <= 122))
-		return (1);
-	return (0);
-}
